6bd8506121199b77c141d09be5835ae087bd07d0
numero28 <90471558+numero28@users.noreply.github.com>
2024-11-26 08:00:09 +0000

    Update mathematica.py

--- mathematica.py
+++ mathematica.py
@@ -5,12 +5,24 @@ Spyder Editor
 This is a temporary script file.
 """
 
-def factorial(n):
+#Iterative factorial
+def factorial_iter(n):
     fact = 1
     for i in range(2,n+1):
         fact = fact * i
     return fact    
 
 numero = int(input('Enter number'))
-fact = factorial(numero)
+fact = factorial_iter(numero)
 print(f'The factorial of {numero} is {fact}')
+
+#Recursive factorial
+def factorial_recur(n):
+    if n <=1:
+        return 1
+    return n * factorial_recur(n - 1)
+
+def fibbonacci_recur(n):
+    if n <= 1:
+        return n
+    return fibbonacci_recur(n - 1) + fibbonacci_recur(n - 2)    
