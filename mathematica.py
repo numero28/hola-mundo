8eb2c16e13e262ed05e8a98c15a4f6abca0fc72c
numero28 <90471558+numero28@users.noreply.github.com>
2024-11-28 23:00:23 +0000

    Update mathematica.py

--- mathematica.py
+++ mathematica.py
@@ -38,3 +38,10 @@ def fibbonacci_iter(n):
         fib0 = fib1
         fib1 = fib
     return fib
+
+#1
+n = int(input('Enter n: '))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,'', end='')
+    print()
